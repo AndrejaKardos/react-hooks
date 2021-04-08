84fca50e8e3d061e963b8afb50eb7dcdbdca0aad
Andie Kardos <andreja.kardos@conserveitiot.com>
2021-04-09 09:45:40 +1000

    Set up buttons in LearnReducer component

--- src/LearnReducer.jsx
+++ src/LearnReducer.jsx
@@ -1,8 +1,22 @@
 import React, { useState } from 'react' 
 
 function LearnReducer() {
+    const [count, setCount] = useState(0)
+
+    function increment() {
+        setCount(prevCount => prevCount + 1)
+    }
+
+    function decrement() {
+        setCount(prevCount => prevCount - 1)
+    }
+
     return (
-        <div></div>
+        <>
+            <button onClick={decrement}>-</button>
+            <span>{count}</span>
+            <button onClick={increment}>+</button>
+        </>
     )
 }
 
