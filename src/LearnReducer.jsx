d5805756a9bb9fd64a912332492a32b9307fda66
Andie Kardos <andreja.kardos@conserveitiot.com>
2021-04-09 09:58:40 +1000

    Included useReducer hook in LearnReducer

--- src/LearnReducer.jsx
+++ src/LearnReducer.jsx
@@ -1,14 +1,35 @@
-import React, { useState } from 'react' 
+import React, { useState, useReducer } from 'react' 
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'increment': 
+            return { count: state.count + 1 }
+        case 'decrement':
+            return { count: state.count - 1}
+        default:
+            return state
+    }
+    
+}
 
 function LearnReducer() {
+    const [state, dispatch] = useReducer(reducer, { count: 0})
     const [count, setCount] = useState(0)
 
-    function increment() {
+    function oldIncrement() {
         setCount(prevCount => prevCount + 1)
     }
 
-    function decrement() {
+    function oldDecrement() {
         setCount(prevCount => prevCount - 1)
+    }
+
+    function increment() {
+        dispatch({ type: 'increment' })
+    }
+
+    function decrement() {
+        dispatch({ type: 'decrement' })
     }
 
     return (
